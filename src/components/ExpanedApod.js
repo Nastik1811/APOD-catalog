0fc64df32a76fc9752f9b4b149550e3efd48a047
Nastik1811 <lugovskaya.1999@mail.ru>
2020-06-18 09:42:26 +0300

    Add pretty loader

--- src/components/ExpanedApod.js
+++ src/components/ExpanedApod.js
@@ -4,10 +4,8 @@ import Loader from './Loader'
 
 const ExpanedApod = ({date, onDismiss}) => {
     const [apod, setApod] = useState(null)
-    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        setLoading(true)
         fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&date=${date}&hd=true`)
             .then(response => {
                 if(response.status === 200){
@@ -17,15 +15,12 @@ const ExpanedApod = ({date, onDismiss}) => {
                 }
             })
             .then(setApod)
-            .then(() => setLoading(false))
             .catch(() => alert("Something went wrong. Try to pick another date."))
         },[date])
 
     return(
         <ModalView onDismiss={onDismiss}>
-            {loading ? 
-                <Loader/> : 
-                <img 
+            {apod && <img 
                     src={apod.hdurl} 
                     alt={apod.title} 
                     className="modal-image"
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import  './Loader.css'
 
 const Loader = () => {
+    // return(
+    //     <div className="loader-container">
+    //              Loading...
+    //     </div>
+    // )
     return(
-        <div className="loader-container">
-                 Loading...
+        <div className="loader">
+                <div className="animation-container">
+                <div className="animation-group">
+                    <span className="loader-component"></span>
+                    <span className="loader-component"></span>
+                    <span className="loader-component"></span>
+                    <span className="loader-component"></span>
+                </div>
+            </div>
         </div>
     )
 }

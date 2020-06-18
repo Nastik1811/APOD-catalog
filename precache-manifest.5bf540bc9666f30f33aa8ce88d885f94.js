57a81756e7167ae8a600916795a17e4d6ad3ae82
Nastik1811 <lugovskaya.1999@mail.ru>
2020-06-18 09:56:49 +0300

    Updates

---
+++ precache-manifest.5bf540bc9666f30f33aa8ce88d885f94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da2221d69f18613656db747509c28f0",
+    "url": "/APOD-catalog/index.html"
+  },
+  {
+    "revision": "60b0dd1e7c8d3aa503ee",
+    "url": "/APOD-catalog/static/css/main.39ee3a0f.chunk.css"
+  },
+  {
+    "revision": "436927babc2ab7135334",
+    "url": "/APOD-catalog/static/js/2.eb3c3855.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/APOD-catalog/static/js/2.eb3c3855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b0dd1e7c8d3aa503ee",
+    "url": "/APOD-catalog/static/js/main.a22afd08.chunk.js"
+  },
+  {
+    "revision": "364e054fbac9f2d52ad2",
+    "url": "/APOD-catalog/static/js/runtime-main.1d192ab8.js"
+  },
+  {
+    "revision": "c2488bfda16e6a811f62414da90e51e4",
+    "url": "/APOD-catalog/static/media/Comfortaa-Regular.c2488bfd.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/APOD-catalog/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
